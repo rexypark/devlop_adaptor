22c8dfa35304fc03c3a03173a1e2f419af88e663
200401 <200401@DESKTOP-REXY>
2020-04-17 18:34:21 +0900

    site_

---
+++ site-example-atp/patterns/FILE_FILE_SEND_01/Starter.java
@@ -0,0 +1,24 @@
+package FILE_FILE_SEND_01;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Starter {
+	
+	public static void main(String[] args) throws Exception {
+        DOMConfigurator.configureAndWatch("log4j.xml");
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+        BeanDefinitionRegistry  registry = (BeanDefinitionRegistry) ctx.getAutowireCapableBeanFactory();
+        registry.removeBeanDefinition("adaptorBean");
+        
+        
+        
+	}
+	
+
+
+
+}
